cc89633b1ed4e41423710674be82452d8b06eb07
Miguel De La Mora <migueldelamora@Miguels-MacBook-Pro.local>
2026-04-01 09:56:00 -0600

    Updating Starter

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../app/globals.css";
 
+export const metadata = {
+  title: "Human Starter",
+  description: "Boilerplate para desarrollo rápido de sitios web",
+};
 
 export default function RootLayout({
   children,
@@ -11,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="font-sans antialiased">
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
