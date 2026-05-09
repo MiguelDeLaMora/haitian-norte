c2e78fbff60b7aa582c583dd202037e72e5fb662
Miguel De La Mora <migueldelamora@Miguels-MacBook-Pro.local>
2026-05-08 18:41:08 -0600

    debugging

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com",
-              "style-src 'self' 'unsafe-inline'",
-              "frame-src https://challenges.cloudflare.com",
-              "connect-src 'self' https://challenges.cloudflare.com",
-              "img-src 'self' data: blob:",
-              "font-src 'self' data:",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig;
